429b3abd36ee5bdd14b0e022066a918b80f1fe38
Alex <Hockeymorozov@yandex.ru>
2021-05-29 13:22:23 +0300

    add README

---
+++ urls.py
@@ -0,0 +1,43 @@
+"""candel URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from candels.views import index_view, CandelListView, AboutTemplateView, CandelDetailView, CandelCreateView, CandelUpdateView, CandelDeleteView, ContactTemplateView
+from user.views import RegisterView, AuthView
+import debug_toolbar
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
+    path('', CandelListView.as_view()),
+    path('about/', AboutTemplateView.as_view()),
+    path('candel/<int:pk>/', CandelDetailView.as_view()),
+    path('update/<int:pk>/', CandelUpdateView.as_view()),
+    path('delete_confirm/<int:pk>/', CandelDeleteView.as_view()),
+    path('create/', CandelCreateView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('candels/', index_view),
+    path('login/', AuthView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('cart/', include(('cart.urls', 'cart'))),
+    path('contact/', ContactTemplateView.as_view()),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
